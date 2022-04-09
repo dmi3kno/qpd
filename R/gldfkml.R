d107f99aa58a7dcfd632c5ca02f33137b29540bc
Dmytro Perepolkin <dperepolkin@gmail.com>
2022-04-09 11:41:15 +0200

    added validity check for both GLDs

--- R/gldfkml.R
+++ R/gldfkml.R
@@ -12,8 +12,9 @@
 #'
 #' @examples
 #' p_grd <- make_pgrid()
-#' qGLDfkml(p_grd, 1, -1, -1/8, -1/32)
+#' qGLDfkml(p_grd, 1, 1, -1/8, -1/32)
 qGLDfkml <- function(u, l1, l2, l3, l4){
+  stopifnot("l2 must be non-negative!"=(l2>=0))
   l4t <- ifelse(l4==0,log(1-u),((1-u)^l4-1)/l4)
   l3t <- ifelse(l3==0,log(u),(u^l3-1)/l3)
 
@@ -23,6 +24,7 @@ qGLDfkml <- function(u, l1, l2, l3, l4){
 #' @rdname GLDfkml
 #' @export
 fGLDfkml <- function(u, l1, l2, l3, l4){
+  stopifnot("l2 must be non-negative!"=(l2>=0))
   l3t <- ifelse(l3==0,1/u,u^(l3-1))
   l4t <- ifelse(l4==0,1/(1-u),(1-u)^(l4-1))
 
@@ -53,7 +55,7 @@ rGLDfkml <- function(n, l1, l2, l3, l4){
 #' @rdname GLDfkml
 #' @export
 pGLDfkml <- function(q, l1, l2, l3, l4, n_grid=50L, s_grid=5L, tol=1e-15, maxiter=1e3){
-
+  stopifnot("l2 must be non-negative!"=(l2>=0))
   afun <- function(u, q, l1, l2, l3, l4) {q - qGLDfkml(u, l1, l2, l3, l4)}
   p_grd <- sort(c(tol, qpd::make_pgrid(n=n_grid, s=s_grid), 1-tol))
   q_grd <- qGLDfkml(p_grd, l1, l2, l3, l4)
@@ -76,8 +78,7 @@ pGLDfkml <- function(q, l1, l2, l3, l4, n_grid=50L, s_grid=5L, tol=1e-15, maxite
 
 #' @export
 #' @rdname GLDfkml
-is_GLDfkml_valid <- function(l1, l2, l3, l4, n_grid=100L, s_grid=2L){
-  grd <- make_pgrid(n_grid, s_grid)
-  all(fGLDfkml(grd, l1,l2,l3,l4)>0)
+is_GLDfkml_valid <- function(l1, l2, l3, l4){
+  (l2>=0)
 }
 
--- R/gldrs.R
+++ R/gldrs.R
@@ -12,6 +12,7 @@
 #'
 #' @examples
 #' p_grd <- make_pgrid()
+#' is_GLDrs_valid(1, -1, -1/8, -1/32)
 #' qGLDrs(p_grd, 1, -1, -1/8, -1/32)
 qGLDrs <- function(u, l1, l2, l3, l4){
   res <- l1+(u^l3-(1-u)^l4)/l2
@@ -71,8 +72,19 @@ pGLDrs <- function(q, l1, l2, l3, l4, n_grid=50L, s_grid=5L, tol=1e-15, maxiter=
 
 #' @export
 #' @rdname GLDrs
-is_GLDrs_valid <- function(l1, l2, l3, l4, n_grid=100L, s_grid=2L){
-  grd <- make_pgrid(n_grid, s_grid)
-  all(fGLDrs(grd, l1,l2,l3,l4)>0)
+is_GLDrs_valid <- function(l1, l2, l3, l4){
+  if((l3<=-1) && (l4>=1)) return(TRUE) # region 1
+  if((l3>=1) && (l4<=-1)) return(TRUE) # region 2
+  if((l3>=0) && (l4>=0)) return(TRUE) # region 3
+  if((l3<=0) && (l4<=0)) return(TRUE) # region 4
+  if((l3<0) && (l4>0) && (l4<1)) return(FALSE) # region V1
+  if((l3>0) && (l3<1) && (l4<0)) return(FALSE) # region V2
+  if((l3>-1) && (l3<0) && (l4>1))
+    if((((1-l3)^(1-l3))/((l4-l3)^(l4-l3))*(l4-1)^(l4-1))<(-l3/l4))
+      return(TRUE) # valid section of region V3
+  if((l3>1) && (l4>-1) && (l4<0))
+    if((((1-l4)^(1-l4))/((l3-l4)^(l3-l4))*(l3-1)^(l3-1))<(-l4/l3))
+      return(TRUE) # valid section of region V4
+  return(FALSE)
 }
 

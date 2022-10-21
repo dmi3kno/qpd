4e61eba793fb205ee60c414f7f1ddf10414b4f86
Dmytro Perepolkin <dperepolkin@gmail.com>
2022-10-21 14:14:10 +0200

    change the internal myerson_rba functions to worth with q3-q2 and q2-q1

--- R/Myerson.R
+++ R/Myerson.R
@@ -1,18 +1,20 @@
 #' @keywords internal
 sMyerson_rba <- function(p,r,b,alpha){
+  bt <- r/b
   qn1ma <-stats::qnorm(1-alpha)
   k <- stats::qnorm(p)/qn1ma
-  if(b==1) return(r*k)
-  r*(b^k-1)/(b-1)
+  if(bt==1) return(r*k)
+  r*(bt^k-1)/(bt-1)
 }
 
 fMyerson_rba <- function(p,r,b,alpha,log){
+  bt <- r/b
   qn1ma <-stats::qnorm(1-alpha)
   k <- stats::qnorm(p)/qn1ma
-  if(b==1){
+  if(bt==1){
     res <- r*fnorm(p)/qn1ma
   } else {
-    res <- r*log(b)*(b^k)*fnorm(p)/(b-1)/qn1ma
+    res <- r*log(bt)*(bt^k)*fnorm(p)/(bt-1)/qn1ma
   }
   if(log) return(log(res))
   res
@@ -38,7 +40,7 @@ fMyerson_rba <- function(p,r,b,alpha,log){
 #' qMyerson(0.25, 10, 20, 40)
 qMyerson <- function(p, q1,q2,q3, alpha=0.25){
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
   q2+sMyerson_rba(p,r,b,alpha)
 }
 
@@ -49,7 +51,7 @@ qMyerson <- function(p, q1,q2,q3, alpha=0.25){
 #' fMyerson(0.25, 10, 20, 40)
 fMyerson <- function(p, q1,q2,q3, alpha=0.25, log=FALSE){
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
   fMyerson_rba(p,r,b, alpha,log)
 }
 
@@ -84,14 +86,15 @@ rMyerson <- function(n,q1,q2,q3,alpha=0.25){
 dMyerson <- function(x, q1,q2,q3, alpha=0.25, log=FALSE){
   # Probability density function
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
+  bt <- r/b
   qn1ma <-stats::qnorm(1-alpha)
-  if(b==1){
+  if(bt==1){
     res <- stats::dnorm(x, mean=q2, sd=r/qn1ma)
   }else{
-    psi <- qn1ma*(log1p((x-q2)*(b-1)/r)/log(b))
-    num <- qn1ma*(b-1)
-    den <- (r+(x-q2)*(b-1))*log(b)
+    psi <- qn1ma*(log1p((x-q2)*(bt-1)/r)/log(bt))
+    num <- qn1ma*(bt-1)
+    den <- (r+(x-q2)*(bt-1))*log(bt)
     res <- num/den*stats::dnorm(psi)
   }
   if(log) return(log(res))
@@ -110,14 +113,15 @@ dMyerson <- function(x, q1,q2,q3, alpha=0.25, log=FALSE){
 pMyerson <- function(q, q1,q2,q3, alpha=0.25){
   # cumulative density
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
+  bt <- r/b
   qn1ma <-stats::qnorm(1-alpha)
 
-  if(b==1)
+  if(bt==1)
     return(stats::pnorm(q, mean=q2, sd=r/qn1ma))
 
-  nom <- log1p((q-q2)*(b-1)/r)
-  den <- log(b)
+  nom <- log1p((q-q2)*(bt-1)/r)
+  den <- log(bt)
   psi <- qn1ma*(nom/den)
   stats::pnorm(psi)
 }
@@ -125,19 +129,21 @@ pMyerson <- function(q, q1,q2,q3, alpha=0.25){
 ########################### LOGIT MYERSON ###################################
 
 slogitMyerson_rba <- function(p,r,b,alpha){
+  bt <- r/b
   qn1ma <-logit(1-alpha)
   k <- logit(p)/qn1ma
-  if(b==1) return(r*k)
-  r*(b^k-1)/(b-1)
+  if(bt==1) return(r*k)
+  r*(bt^k-1)/(bt-1)
 }
 
 flogitMyerson_rba<- function(p,r,b,alpha,log){
+  bt <- r/b
   qn1ma <-logit(1-alpha)
   k <- logit(p)/qn1ma
-  if(b==1){
+  if(bt==1){
     res <- r/(p*(1-p)*qn1ma)
   } else {
-    res <- r*log(b)*(b^k)/(p*(1-p)*(b-1)*qn1ma)
+    res <- r*log(bt)*(bt^k)/(p*(1-p)*(bt-1)*qn1ma)
   }
 
   if(log) return(log(res))
@@ -164,7 +170,7 @@ flogitMyerson_rba<- function(p,r,b,alpha,log){
 #' qlogitMyerson(0.25, 10, 20, 40)
 qlogitMyerson <- function(p, q1,q2,q3, alpha=0.25){
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
   q2+slogitMyerson_rba(p,r,b,alpha)
 }
 
@@ -175,7 +181,7 @@ qlogitMyerson <- function(p, q1,q2,q3, alpha=0.25){
 #' flogitMyerson(0.25, 10, 20, 40)
 flogitMyerson <- function(p, q1,q2,q3, alpha=0.25, log=FALSE){
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
   flogitMyerson_rba(p,r,b,alpha,log)
 }
 
@@ -214,13 +220,14 @@ rlogitMyerson <- function(n,q1,q2,q3,alpha=0.25){
 plogitMyerson <- function(q, q1,q2,q3, alpha=0.25){
   # cumulative density
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
+  bt <- r/b
   qn1ma <-logit(1-alpha)
-  if(b==1)
+  if(bt==1)
     return(invlogit(qn1ma*(q-q2)/r))
 
-  nom <- log1p((q-q2)*(b-1)/r)
-  den <- log(b)
+  nom <- log1p((q-q2)*(bt-1)/r)
+  den <- log(bt)
   psi <- qn1ma*(nom/den)
   invlogit(psi)
 }
@@ -234,15 +241,16 @@ plogitMyerson <- function(q, q1,q2,q3, alpha=0.25){
 dlogitMyerson <- function(x, q1,q2,q3, alpha=0.25, log=FALSE){
   # Probability density function
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
+  bt <- r/b
   qn1ma <-logit(1-alpha)
   psi <- qn1ma*(x-q2)/r
-  if(b==1){
+  if(bt==1){
     res <- stats::dlogis(psi)
   }else{
-    psi <- qn1ma*(log(1+(x-q2)*(b-1)/r)/log(b))
-    num <- qn1ma*(b-1)
-    den <- (r+(x-q2)*(b-1))*log(b)
+    psi <- qn1ma*(log(1+(x-q2)*(bt-1)/r)/log(bt))
+    num <- qn1ma*(bt-1)
+    den <- (r+(x-q2)*(bt-1))*log(bt)
     res <- num/den*stats::dlogis(psi)
   }
 
@@ -252,19 +260,21 @@ dlogitMyerson <- function(x, q1,q2,q3, alpha=0.25, log=FALSE){
 
 ####################### CAUCHY MYERSON ###################################
 scauchyMyerson_rba <- function(p,r,b,alpha){
+  bt <- r/b
   qn1ma <-tan(pi*(0.5-alpha))
   k <- tan(pi*(p-0.5))/qn1ma
-  if(b==1) return(r*k)
-  r*(b^k-1)/(b-1)
+  if(bt==1) return(r*k)
+  r*(bt^k-1)/(bt-1)
 }
 
 fcauchyMyerson_rba <- function(p,r,b,alpha,log){
+  bt <- r/b
   qn1ma <-tan(pi*(0.5-alpha))
   k <- tan(pi*(p-0.5))/qn1ma
-  if(b==1){
+  if(bt==1){
     res <- r*pi*(1+tan(pi*(p-0.5))^2)/qn1ma
   } else {
-    res <- r*log(b)*(b^k)*pi*(1+tan(pi*(p-0.5))^2)/((b-1)*qn1ma)
+    res <- r*log(bt)*(bt^k)*pi*(1+tan(pi*(p-0.5))^2)/((bt-1)*qn1ma)
   }
 
   if(log) return(log(res))
@@ -290,7 +300,7 @@ fcauchyMyerson_rba <- function(p,r,b,alpha,log){
 #' qcauchyMyerson(0.25, 10, 20, 40)
 qcauchyMyerson <- function(p, q1,q2,q3, alpha=0.25){
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
   q2+scauchyMyerson_rba(p,r,b,alpha)
 }
 
@@ -301,7 +311,7 @@ qcauchyMyerson <- function(p, q1,q2,q3, alpha=0.25){
 #' fcauchyMyerson(0.25, 10, 20, 40)
 fcauchyMyerson <- function(p, q1,q2,q3, alpha=0.25, log=FALSE){
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
   fcauchyMyerson_rba(p,r,b,alpha,log)
 }
 
@@ -340,13 +350,14 @@ rcauchyMyerson <- function(n,q1,q2,q3,alpha=0.25){
 pcauchyMyerson <- function(q, q1,q2,q3, alpha=0.25){
   # cumulative density
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
+  bt <- r/b
   qn1ma <-tan(pi*(0.5-alpha))
-  if(b==1)
+  if(bt==1)
     return(stats::pcauchy(qn1ma*(q-q2)/r))
 
-  nom <- log1p((q-q2)*(b-1)/r)
-  den <- log(b)
+  nom <- log1p((q-q2)*(bt-1)/r)
+  den <- log(bt)
   psi <- qn1ma*(nom/den)
   stats::pcauchy(psi)
 }
@@ -360,15 +371,16 @@ pcauchyMyerson <- function(q, q1,q2,q3, alpha=0.25){
 dcauchyMyerson <- function(x, q1,q2,q3, alpha=0.25, log=FALSE){
   # Probability density function
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
+  bt <- r/b
   qn1ma <-tan(pi*(0.5-alpha))
   psi <- qn1ma*(x-q2)/r
-  if(b==1){
+  if(bt==1){
     res <- stats::dcauchy(psi)
   }else{
-    psi <- qn1ma*(log(1+(x-q2)*(b-1)/r)/log(b))
-    num <- qn1ma*(b-1)
-    den <- (r+(x-q2)*(b-1))*log(b)
+    psi <- qn1ma*(log(1+(x-q2)*(bt-1)/r)/log(bt))
+    num <- qn1ma*(bt-1)
+    den <- (r+(x-q2)*(bt-1))*log(bt)
     res <- num/den*stats::dcauchy(psi)
   }
 
@@ -379,19 +391,21 @@ dcauchyMyerson <- function(x, q1,q2,q3, alpha=0.25, log=FALSE){
 ######################### SECH MYERSON #######################################
 
 ssechMyerson_rba <- function(p,r,b,alpha){
+  bt <- r/b
   qn1ma <-2/pi*log(tan(pi/2*(1-alpha))) #sech QF
   k <- 2/pi*log(tan(pi/2*p))/qn1ma
-  if(b==1) return(r*k)
-  r*(b^k-1)/(b-1)
+  if(bt==1) return(r*k)
+  r*(bt^k-1)/(bt-1)
 }
 
 fsechMyerson_rba <- function(p,r,b,alpha,log){
+  bt <- r/b
   qn1ma <-2/pi*log(tan(pi/2*(1-alpha))) #sech QF
   k <- 2/pi*log(tan(pi/2*p))/qn1ma
-  if(b==1){
+  if(bt==1){
     res <- r*pi*(1+tan(pi*p/2)^2)/(pi*qn1ma*tan(pi*p/2))
   } else {
-    res <- r*log(b)*(b^k)*pi*(1+tan(pi*p/2)^2)/((b-1)*pi*qn1ma*tan(pi*p/2))
+    res <- r*log(bt)*(bt^k)*pi*(1+tan(pi*p/2)^2)/((bt-1)*pi*qn1ma*tan(pi*p/2))
   }
   if(log) return(log(res))
   res
@@ -417,7 +431,7 @@ fsechMyerson_rba <- function(p,r,b,alpha,log){
 #' qsechMyerson(0.25, 10, 20, 40)
 qsechMyerson <- function(p, q1,q2,q3, alpha=0.25){
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
   q2+ssechMyerson_rba(p,r,b,alpha)
 }
 
@@ -428,7 +442,7 @@ qsechMyerson <- function(p, q1,q2,q3, alpha=0.25){
 #' fsechMyerson(0.25, 10, 20, 40)
 fsechMyerson <- function(p, q1,q2,q3, alpha=0.25, log=FALSE){
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
   qn1ma <-2/pi*log(tan(pi/2*(1-alpha)))
   k <- 2/pi*log(tan(pi/2*p))/qn1ma
   fsechMyerson_rba(p,r,b,alpha,log)
@@ -469,13 +483,14 @@ rsechMyerson <- function(n,q1,q2,q3,alpha=0.25){
 psechMyerson <- function(q, q1,q2,q3, alpha=0.25){
   # cumulative density
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
+  bt <- r/b
   qn1ma <-2/pi*log(tan(pi/2*(1-alpha)))
-  if(b==1)
+  if(bt==1)
     return(2/pi*atan(exp(pi/2*(qn1ma*(q-q2)/r))))
 
-  nom <- log1p((q-q2)*(b-1)/r)
-  den <- log(b)
+  nom <- log1p((q-q2)*(bt-1)/r)
+  den <- log(bt)
   psi <- qn1ma*(nom/den)
   2/pi*atan(exp(pi/2*psi))
 }
@@ -489,15 +504,16 @@ psechMyerson <- function(q, q1,q2,q3, alpha=0.25){
 dsechMyerson <- function(x, q1,q2,q3, alpha=0.25, log=FALSE){
   # Probability density function
   r <- (q3-q2)
-  b <- r/(q2-q1)
+  b <- (q2-q1)
+  bt <- r/b
   qn1ma <-2/pi*log(tan(pi/2*(1-alpha)))
   psi <- qn1ma*(x-q2)/r
-  if(b==1) {
+  if(bt==1) {
     res <- 1/2*sech(pi/2*psi)
   } else {
-    psi <- qn1ma*(log1p((x-q2)*(b-1)/r)/log(b))
-    num <- qn1ma*(b-1)
-    den <- (r+(x-q2)*(b-1))*log(b)
+    psi <- qn1ma*(log1p((x-q2)*(bt-1)/r)/log(bt))
+    num <- qn1ma*(bt-1)
+    den <- (r+(x-q2)*(bt-1))*log(bt)
     res <- num/den*1/2*sech(pi/2*psi)
   }
   if(log) return(log(res))
